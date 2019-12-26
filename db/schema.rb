cc3aff61adc9b63263065830c3bf4934a940252c
manish kumar <manikuma0405@gmail.com>
2019-12-26 19:58:08 +0900

    アソシエーションを定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_102601) do
+ActiveRecord::Schema.define(version: 2019_12_26_105241) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_12_26_102601) do
     t.integer "minute_on_foot", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "property_id"
+    t.index ["property_id"], name: "index_stations_on_property_id"
   end
 
 end
