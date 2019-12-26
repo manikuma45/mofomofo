1fb1b160cc207d87ac63c8a06b36e90e0193a812
manish kumar <manikuma0405@gmail.com>
2019-12-26 19:10:39 +0900

    cssの修正とネストフォームの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_12_26_105241) do
+=======
+ActiveRecord::Schema.define(version: 2019_12_26_094512) do
+>>>>>>> 66b6dc6... scaffoldによる物件登録機能の作成
 
   create_table "properties", force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_105241) do
     t.datetime "updated_at", null: false
   end
 
+<<<<<<< HEAD
   create_table "stations", force: :cascade do |t|
     t.string "line_name", null: false
     t.string "sta_name", null: false
@@ -32,4 +37,6 @@ ActiveRecord::Schema.define(version: 2019_12_26_105241) do
     t.index ["property_id"], name: "index_stations_on_property_id"
   end
 
+=======
+>>>>>>> 66b6dc6... scaffoldによる物件登録機能の作成
 end
