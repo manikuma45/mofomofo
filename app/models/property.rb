6db35d02b000a42d1d0cd2f3b4793a80a8cb9f99
manish kumar <manikuma0405@gmail.com>
2019-12-26 22:19:28 +0900

    viewとバリデーションの追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ApplicationRecord
 	has_many :stations, inverse_of: :property, dependent: :destroy
 	accepts_nested_attributes_for :stations, allow_destroy: true
+	validates :name,  presence: true
+	validates :rent,  presence: true, numericality: {greater_than_or_equal_to: 0}
+	validates :address,  presence: true
+	validates :old,  presence: true,numericality: {greater_than_or_equal_to: 0}
 end
